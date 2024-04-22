ec32638d9ff55bf4707eb437487663b1f459c1ef
qarisoft <131317208+qarisoft@users.noreply.github.com>
2024-04-22 06:57:17 +0300

    salah1

--- app/Traits/DefaultsResource.php
+++ app/Traits/DefaultsResource.php
@@ -7,19 +7,17 @@ use Illuminate\Support\Facades\DB;
 
 trait DefaultsResource
 {
-//    public static function getEloquentQuery() : Builder{
-//        return parent::getEloquentQuery()->orWhere('default',true);
-//    }
-    public static function getEloquentQuery() : Builder{
-        $q= parent::getEloquentQuery();
+    public static function getEloquentQuery(): Builder
+    {
+        $q = parent::getEloquentQuery();
 
-        $q->orwhereExists(function ( $query) {
+        $q->orwhereExists(function ($query) {
             $query->select(DB::raw(1))
                 ->from('defaults')
-                ->whereColumn('defaults.default_type','=', static::getModel())
-                ->whereColumn('defaults.default_id','=', 'id')
-            ;
+                ->where('defaults.default_type', '=', static::getModel())
+                ->where('defaults.default_id', '=', 'id');
         });
+
         return $q;
     }
 }
