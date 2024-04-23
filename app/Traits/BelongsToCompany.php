369d7e74daeaeeed2436beb56792a5435dfe4c20
qarisoft <131317208+qarisoft@users.noreply.github.com>
2024-04-23 07:38:07 +0300

    init10

---
+++ app/Traits/BelongsToCompany.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Collection;
+
+trait Defaults
+{
+    public static function defaults(): Collection
+    {
+        return static::query()->where('default', true);
+    }
+
+    public function scopeDefault($q)
+    {
+        return $q->where('default', true);
+    }
+
+    public function is_default(): bool
+    {
+        return $this->default ?? false;
+    }
+}
